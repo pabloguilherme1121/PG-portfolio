f79a51e36b2e7d770b87faa4cc3fb0523018f882
PG-Tech <pabloguilherme1121@gmail.com>
2026-09-25 07:05:13 -0300

    feat: register PWA in production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
+import { registerPortfolioPwa } from "./pwa";
 import { startLogin } from "./const";
 import "./index.css";
 
@@ -79,3 +80,5 @@ createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </trpc.Provider>
 );
+
+registerPortfolioPwa();
